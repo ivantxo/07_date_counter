877e2187c4359d5b3255ff256fd27fea78cf6908
IvanR <ivan.ramirezf@gmail.com>
2024-11-13 22:52:37 +1000

    Added old date counter code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Hello</div>;
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  function decreaseStep() {
+    setStep((c) => c - 1);
+  }
+
+  function increaseStep() {
+    setStep((c) => c + 1);
+  }
+
+  function decreaseCount() {
+    setCount((c) => c - step);
+  }
+
+  function increaseCount() {
+    setCount((c) => c + step);
+  }
+
+  let day = count === 0 ? "Today" : `${count} days from today`;
+
+  const today = new Date();
+  today.setDate(today.getDate() + count);
+  return (
+    <div style={{ textAlign: "center", fontSize: "26px" }}>
+      <div>
+        <button onClick={() => decreaseStep()}>-</button> Step {step}{" "}
+        <button onClick={() => increaseStep()}>+</button>
+      </div>
+      <div>
+        <button onClick={() => decreaseCount()}>-</button> Count {count}{" "}
+        <button onClick={() => increaseCount()}>+</button>
+      </div>
+      <div>
+        {day} is {today.toDateString()}
+      </div>
+    </div>
+  );
 }
 
 export default App;
